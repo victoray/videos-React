fa72cb24ac61f1c0d71984437f296994e7301ca9
Victor Ay <viktoray007@gmail.com>
2019-10-13 10:30:44 +0300

    Updates

---
+++ precache-manifest.0fce99e6998fef3770e4009304c3c1ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eccbbe369c3d39d0746aee3adde777b9",
+    "url": "/videos-React/index.html"
+  },
+  {
+    "revision": "9547d6c9f59cfe623634",
+    "url": "/videos-React/static/css/main.1df53d77.chunk.css"
+  },
+  {
+    "revision": "d2274b02ea090db534d6",
+    "url": "/videos-React/static/js/2.99a700a8.chunk.js"
+  },
+  {
+    "revision": "9547d6c9f59cfe623634",
+    "url": "/videos-React/static/js/main.cbf53a77.chunk.js"
+  },
+  {
+    "revision": "d5d43fe58dee62e665e1",
+    "url": "/videos-React/static/js/runtime-main.fbd13a2f.js"
+  }
+]);
